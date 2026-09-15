4291d1467406131b59eeb72d43970e25e25f1041
dsault <35780152+dsault@users.noreply.github.com>
2026-09-16 07:38:25 +0800

    Remove unused poster loader

--- assets/pictures/event-poster.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var parts = window.__EVENT_POSTER_PARTS || [];
-  if (!parts.length) return;
-  var src = "data:image/jpeg;base64," + parts.join("");
-  document.querySelectorAll("img[data-event-poster]").forEach(function (img) {
-    img.src = src;
-  });
-})();
